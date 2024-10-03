061c4a1504fad0f28875b8ba387b33892f1dd50c
manfroiweb <manfroiweb@gmail.com>
2024-10-03 02:08:08 -0300

    feat: ajustando chamada de api player

--- assets/js/script.js
+++ assets/js/script.js
@@ -230,7 +230,7 @@ function actionModalParams (){
 
 actionModalParams ()
 
-const player = new Plyr('video', {captions: {active: true}});
+const player = new Plyr('video');
 
 // Expose player so it can be used from the console
 window.player = player;
